01138af8ca9b2bf3fbd5496cea7ec2e845c22e92
nataekoon <smathj@naver.com>
2023-09-18 07:57:10 +0900

    Array - 4. 피보나치 수열

---
+++ src/chapter/array/m04/Main_T.java
@@ -0,0 +1,26 @@
+package chapter.array.m04;
+
+import java.util.*;
+
+
+
+public class Main_T {
+	public int[] solution(int n){
+		int[] answer=new int[n];
+		answer[0]=1;
+		answer[1]=1;
+		for(int i=2; i<n; i++){
+			answer[i]=answer[i-2]+answer[i-1];
+		}
+		return answer;
+	}
+	public static void main(String[] args){
+        Main_T T = new Main_T();
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		for(int x :T.solution(n)) System.out.print(x+" ");
+	}
+}
+
+
+
