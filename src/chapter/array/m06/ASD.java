2cff1e32858cfa384c6f7a0571c9a05769402f0b
nataekoon <smathj@naver.com>
2023-09-29 23:57:13 +0900

    Array - 6. 뒤집은 소수

---
+++ src/chapter/array/m06/ASD.java
@@ -0,0 +1,10 @@
+package chapter.array.m06;
+
+public class ASD {
+
+    public static void main(String[] args) {
+
+        System.out.println(321 / 10);
+
+    }
+}
--- src/chapter/array/m06/Main.java
+++ src/chapter/array/m06/Main.java
@@ -9,26 +9,45 @@ import java.util.Scanner;
 public class Main {
 	public boolean isPrime(int num){
 		if(num==1) return false;
+
 		for(int i=2; i<num; i++){
+			// 약수가 있으면 거짓
 			if(num%i==0) return false;
 		}
+
+		// 약수가 없으면 소수가 맞다
 		return true;
 	}
 
 	public ArrayList<Integer> solution(int n, int[] arr){
+
 		ArrayList<Integer> answer = new ArrayList<>();
+
 		for(int i=0; i<n; i++){
+			// 기본값
 			int tmp=arr[i];
+			// 뒤집은 값
 			int res=0;
-			while(tmp>0){
+
+			// 뒤집는 로직, 0 보다 클때만
+			while(tmp>0) {
+
+				// 마지막 수
 				int t=tmp%10;
+
+				// 뒤집은 수
 				res=res*10+t;
+
+				// 그 다음 수
 				tmp=tmp/10;
 			}
+
+			// 소수면 add
 			if(isPrime(res)) answer.add(res);
 		}
 		return answer;
 	}
+
 	public static void main(String[] args){
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
