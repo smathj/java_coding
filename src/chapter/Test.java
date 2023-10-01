583c3cd4c12c7be4a665c25e60235da91be6ea07
nataekoon <smathj@naver.com>
2023-10-01 21:01:17 +0900

    Array - 10: 봉우리

---
+++ src/chapter/Test.java
@@ -0,0 +1,16 @@
+package chapter;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+
+            System.out.println("i = " + i);
+            if (i == 1) {
+                break;
+            }
+        }
+
+    }
+}
