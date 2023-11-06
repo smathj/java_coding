6e1e5692b7c72ea6ac0d42a3990ffc902ed7ed7d
nataekoon <smathj@naver.com>
2023-11-07 08:31:53 +0900

    Stack, Queue - 2. 괄호문자제거

--- src/chapter/stack_queue/m01/Main_T.java
+++ src/chapter/stack_queue/m01/Main_T.java
@@ -19,9 +19,15 @@ public class Main_T {
 		String answer="YES";
 		Stack<Character> stack=new Stack<>();
 		for(char x : str.toCharArray()){
-			if(x=='(') stack.push(x);
-			else{
+
+			if( x=='(' ) stack.push(x);
+			else {
 				if(stack.isEmpty()) return "NO";
+				/*
+				이 스택의 맨 위에 있는 개체를 제거하고 해당 개체를 이 함수의 값으로 반환합니다.
+				반환: 이 스택의 맨 위에 있는 개체( Vector 개체의 마지막 항목)입니다.
+				던지기: EmptyStackException – 이 스택이 비어 있는 경우.
+				 */
 				stack.pop();
 			}
 		}
