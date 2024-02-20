5c25b558c112c7e0b033c6e3854f9abcd24627a2
smathj <smathj@naver.com>
2024-02-21 08:24:03 +0900

    인프런 - 재귀함수를 이용한 팩토리얼

---
+++ src/chapter/inflearn/recursive_tree_graph/m03/Main.java
@@ -0,0 +1,48 @@
+package chapter.inflearn.recursive_tree_graph.m03;
+
+import java.util.Scanner;
+
+
+/*
+
+5
+
+120
+
+ */
+public class Main {
+
+    public static int value = 1;
+
+    public static void dfs(int n) {
+
+        if (n == 0) {
+            return;
+        } else {
+            dfs(n - 1);
+            value *= n;
+        }
+
+
+    }
+
+
+    public void solution(int n) {
+
+        dfs(n);
+        System.out.println(value);
+
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+
+        T.solution(n);
+    }
+
+}
