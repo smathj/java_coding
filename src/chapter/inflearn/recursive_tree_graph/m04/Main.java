ea0a2ebaaec3c17d2eea5166b2ddc29da4c6ca4b
smathj <smathj@naver.com>
2024-02-28 22:55:44 +0900

    인프런 - DFS 피보나치

---
+++ src/chapter/inflearn/recursive_tree_graph/m04/Main.java
@@ -0,0 +1,53 @@
+package chapter.inflearn.recursive_tree_graph.m04;
+
+import java.util.Scanner;
+
+
+/*
+
+▣ 입력예제 1
+10
+
+▣ 출력예제 1
+1 1 2 3 5 8 13 21 34 55
+
+ */
+public class Main {
+
+    static int count = 1;
+    static int preValue = 0;
+    static int stopCount = 1;
+
+    public static void dfs(int value) {
+
+        // 10 > 10
+        if(count == stopCount) return;
+
+        int nextValue = preValue + value;   // 2
+        count++;
+        preValue = value;
+        System.out.print(nextValue + " ");
+        dfs(nextValue);
+
+
+    }
+
+
+    public void solution(int n) {
+        stopCount = n;
+        System.out.print(1 + " ");
+        dfs(1);
+    }
+
+    public static void main(String[] args) {
+
+        Main T = new Main();
+
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+
+        T.solution(n);
+    }
+
+}
