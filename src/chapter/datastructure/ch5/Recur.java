90a339a2a3a728ff13767cd77a06241e1d5f464f
smathj <smathj@naver.com>
2024-03-17 16:27:05 +0900

    자료구조 - 재귀 ...

---
+++ src/chapter/datastructure/ch5/Recur.java
@@ -0,0 +1,27 @@
+package chapter.datastructure.ch5;
+
+import java.util.Scanner;
+
+public class Recur {
+
+    // * 재귀함수
+    public static void recur(int n) {
+
+        if (n > 0) {
+            recur(n - 1);           // 헤드 재귀
+            System.out.println(n);
+            recur(n - 2);           // 꼬리 재귀
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("정수를 입력하세요: ");
+        int x = stdIn.nextInt();
+
+        recur(x);
+
+    }
+}
---
+++ src/chapter/datastructure/ch5/RecurX1.java
@@ -0,0 +1,31 @@
+package chapter.datastructure.ch5;
+
+import java.util.Scanner;
+
+public class RecurX1 {
+
+
+    /**
+     * 재귀함수
+     * ! 꼬리 재귀 제거
+     */
+    public static void recur(int n) {
+
+        while (n > 0) {
+            recur(n - 1);           // 헤드 재귀
+            System.out.println(n);
+            n = n - 2;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("정수를 입력하세요: ");
+        int x = stdIn.nextInt();
+
+        recur(x);
+
+    }
+}
