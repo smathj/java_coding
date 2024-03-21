caabd80af04451cf04b079ef46f815f2f7bc5d8b
smathj <smathj@naver.com>
2024-03-21 18:41:13 +0900

    패캠 - 11068 회문인 수

---
+++ src/Test3.java
@@ -0,0 +1,14 @@
+public class Test3 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(256/17);
+        System.out.println(256%17);
+
+        System.out.println();
+
+        System.out.println((256/17)/ 17);
+        System.out.println((256/17)% 17);
+    }
+}
--- src/chapter/Test.java
+++ src/chapter/Test.java
@@ -6,18 +6,25 @@ public class Test {
 
     public static void main(String[] args) {
 
+        System.out.println((char) 62);
+
         char c = 'A';
         System.out.println(c);
         System.out.println((int) c);
 
-        String[] valueArr = new String[36];
+        int jinbub = 64;
+        String[] valueArr = new String[jinbub];
 
-        for (int i = 0; i < 36; i++) {
+        for (int i = 0; i < jinbub; i++) {
             if (i >= 10) {
                 valueArr[i] = String.valueOf((char) (65 + i - 10));
             } else {
                 valueArr[i] = String.valueOf(i);
             }
+        }
+
+        for (int i = 0; i < valueArr.length; i++) {
+            System.out.println(i + " 는 : " + valueArr[i]);
         }
 
         for (int i = 0; i < valueArr.length; i++) {
