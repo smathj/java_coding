277fa7c1bae252e9854d78381bc53da4c8ee3031
smathj <smathj@naver.com>
2024-03-23 00:54:01 +0900

    인프런 - DFS 레벨 탐색

--- src/chapter/inflearn/recursive_tree_graph/m07/Main_T.java
+++ src/chapter/inflearn/recursive_tree_graph/m07/Main_T.java
@@ -29,21 +29,35 @@ public class Main_T {
         tree.BFS(tree.root);
     }
 
+
     public void BFS(Node root) {
         Queue<Node> Q = new LinkedList<>();
+        // 최초에 루트 노드를 넣음
         Q.add(root);
+
+        // 레벨
         int L = 0;
+
+        // 큐가 들어있다면
         while (!Q.isEmpty()) {
+
             int len = Q.size();
             System.out.print(L + " : ");
+
             for (int i = 0; i < len; i++) {
                 Node cur = Q.poll();
                 System.out.print(cur.data + " ");
+                // 현재 노드의 왼쪽 자식 노드가 있다면 큐에 저장
                 if (cur.lt != null) Q.add(cur.lt);
+
+                // 현재 노드의 오른쪽 자식 노드가 있다면 큐에 저장
                 if (cur.rt != null) Q.add(cur.rt);
             }
+
+            // 레벨을 증가 시킴
             L++;
             System.out.println();
         }
+
     }
 }
