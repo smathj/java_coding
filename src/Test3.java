2f04f564fbf26586b21667ebf01a95cf8b637f0d
smathj <smathj@naver.com>
2024-03-28 21:45:51 +0900

    알고리즘 - 퀵정렬

--- src/Test3.java
+++ src/Test3.java
@@ -7,9 +7,15 @@ public class Test3 {
         int a, b;
 
         a = b = 10;
+        print(a++, b++);
+        System.out.println("[메서드 동작후]a = " + a);
+        System.out.println("[메서드 동작후]b = " + b);
+
+    }
+
+    public static void print(int a, int b) {
         System.out.println("a = " + a);
         System.out.println("b = " + b);
-
 
     }
 }
---
+++ src/chapter/codingtestInJava/M03.java
@@ -0,0 +1,42 @@
+package chapter.codingtestInJava;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class M03 {
+    public static void main(String[] args) {
+
+
+        System.out.println(Arrays.toString(solution(new int[]{2, 1, 3, 4, 1})));
+        System.out.println(Arrays.toString(solution(new int[]{5, 0, 2, 7})));
+
+
+    }
+
+    public static int[] solution(int[] numbers) {
+
+        HashSet<Integer> hash = new HashSet<>();
+
+        // 서로 다른 인덱스만 뽑아서 조합해야한다
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if(i != j)
+                    hash.add(numbers[i] + numbers[j]);
+            }
+        }
+
+
+        int[] answer = new int[hash.size()];
+        Iterator<Integer> itr = hash.iterator();
+        int i = 0;
+        while (itr.hasNext()) {
+            answer[i] = itr.next();
+            i++;
+        }
+
+        Arrays.sort(answer);
+        return answer;
+    }
+
+}
