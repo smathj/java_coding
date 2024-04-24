bceaf758e2bec102c6f0910685f692b5d7a62b23
smathj <smathj@naver.com>
2024-04-25 00:19:41 +0900

    패캠 - 17232 생명 게임 (구간 합 구하기 5 아이디어 이용해야한다)

--- src/chapter/fastcampus/part1/prefixsum/m04/Main_T.java
+++ src/chapter/fastcampus/part1/prefixsum/m04/Main_T.java
@@ -3,22 +3,34 @@ package chapter.fastcampus.part1.prefixsum.m04;
 import java.util.Scanner;
 
 public class Main_T {
+
+
+    /**
+     * 누적합 배열 생성 메서드
+     * 나는 "드래그 누적 배열" 이라고함
+     */
     static int[][] getPrefixSum(char[][] map) {
         int N = map.length - 1;
         int M = map[0].length - 1;
 
-        // 누적합 배열
+        // 누적합 배열 생성
         int[][] acc = new int[N + 1][M + 1];
 
         // 누적합 로직
         for (int i = 1; i <= N; i++)
             for (int j = 1; j <= M; j++) {
                 int alive = (map[i][j] == '*' ? 1 : 0);
+                // 누적 연산식
                 acc[i][j] = acc[i - 1][j] + acc[i][j - 1] - acc[i - 1][j - 1] + alive;  //! 구간 합 구하기 5 아이디어
             }
         return acc;
     }
 
+    /**
+     * 누적합 배열로 특정 부분의 누적합을 연산할때 사용하는 메서드
+     * 누적합 배열(드래그 누적 배열)로 특정 (r1,c1) -> (r2,c2)의 영역의 누적합 구함
+     *
+     */
     static int getRangeSum(int[][] acc, int r, int c, int K) {
         int r1 = Math.max(r - K, 1);                    // 행의 최소 인덱스는 1 이다
         int c1 = Math.max(c - K, 1);                    // 열의 최소 인덱스는 1 이다
@@ -46,9 +58,15 @@ public class Main_T {
         }
 
         while (T-- > 0) {
+            // 각 시간마다 새롭게 누적합 배열 생성
             int[][] acc = getPrefixSum(map);
+
+            //
             for (int i = 1; i <= N; i++)
+
+                //
                 for (int j = 1; j <= M; j++) {
+                    // 부분합 연산
                     int nearAlive = getRangeSum(acc, i, j, K);
                     if (map[i][j] == '*') {
                         nearAlive--;
