bd33acec50002b8227bc2a13e0233945f2cd22e8
smathj <smathj@naver.com>
2024-06-11 09:16:47 +0900

    쉽게 배우는 알고리즘 문제풀이 m3,4 DFS 최대점수 구하기, 중복 순열

---
+++ src/chapter/자바알고리즘문제풀이입문/dfs_bfs/m03_dfs/Main.java
@@ -0,0 +1,59 @@
+package chapter.자바알고리즘문제풀이입문.dfs_bfs.m03_dfs;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int[] scoreArr;
+    static int[] timeArr;
+    static int N;
+    static int M;
+    static int totalTime;
+    static int totalScore;
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        scoreArr = new int[N];
+        timeArr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            scoreArr[i] = sc.nextInt();  // 점수
+            timeArr[i] = sc.nextInt();  // 시간
+        }
+
+        Main T = new Main();
+        T.DFS(0, 0,0);
+        System.out.println(totalScore);
+
+
+    }
+
+
+    public void DFS(int i, int sumScore, int sumTime) {
+
+        if (sumTime > M) {
+            return;
+        }
+
+
+        if (i == N) {
+            totalScore = Math.max(totalScore, sumScore);
+
+        } else {
+
+            // 현재 문제를 안푼경우
+            DFS(i + 1, sumScore, sumTime);
+
+            // 현재 문제를 푼 경우
+            DFS(i + 1, sumScore + scoreArr[i], sumTime + timeArr[i]);
+        }
+
+
+    }
+}
---
+++ src/chapter/자바알고리즘문제풀이입문/dfs_bfs/m04_dfs/Main_T.java
@@ -0,0 +1,28 @@
+package chapter.자바알고리즘문제풀이입문.dfs_bfs.m04_dfs;
+
+import java.util.Scanner;
+
+public class Main_T {
+    static int[] pm;
+   	static int n, m;
+   	public void DFS(int L){
+   		if(L==m){
+   			for(int x : pm) System.out.print(x+" ");
+   			System.out.println();
+   		}
+   		else{
+   			for(int i=1; i<=n; i++){
+   				pm[L]=i;
+   				DFS(L+1);
+   			}
+   		}
+   	}
+   	public static void main(String[] args){
+        Main_T T = new Main_T();
+   		Scanner kb = new Scanner(System.in);
+   		n=kb.nextInt();
+   		m=kb.nextInt();
+   		pm=new int[m];
+   		T.DFS(0);
+   	}
+}
