5a7252f39399a24b79d96d3ca61390fc9246eb5b
smathj <smathj@naver.com>
2024-07-09 07:20:28 +0900

    [김영한 자바 중급1편 - 날짜와 시간]
    날짜와 시간 연습문제

---
+++ src/main/java/com/coding/study/lecture/김영한/자바중급1편/time/test/TestAdjusters.java
@@ -0,0 +1,21 @@
+package com.coding.study.lecture.김영한.자바중급1편.time.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+public class TestAdjusters {
+    public static void main(String[] args) {
+
+        int year = 2024;
+        int month = 1;
+
+        LocalDate date = LocalDate.of(year, month, 1);
+        DayOfWeek firstDayOfWeek = date.getDayOfWeek();
+        DayOfWeek lastDayOfWeek = date.with(TemporalAdjusters.lastDayOfMonth()).getDayOfWeek();
+        System.out.println("firstDayOfWeek = " + firstDayOfWeek);
+        System.out.println("lastDayOfWeek = " + lastDayOfWeek);
+
+    }
+}
